68e8caaa868e03bd0960f3399dab3d7757974b97
Victor <victorcapelini@hotmail.com>
2018-07-02 16:29:37 -0300

    Adicionada funcionalidade para contar caracteres e palavras utilizando expressões regulares para evitar contagem equivocadas.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
 var frase = $(".frase").text();
-var numeroDePalavras = frase.split(" ").length;
+var numeroDePalavras = frase.split(/\S+/).length-1;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numeroDePalavras);
 
+var campo = $(".campo-digitacao");
+
+campo.on("input",function(){
+    var conteudo = campo.val();
+
+    var quantidadePalavras = conteudo.split(/\S+/).length-1;
+
+    $("#contador-palavras").text(quantidadePalavras);
+    $("#contador-caracteres").text(conteudo.length);
+});
