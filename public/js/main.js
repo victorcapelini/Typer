eff556c153c99af40315a2fde20618cbc0ec40b6
Victor <victorcapelini@hotmail.com>
2018-07-02 17:04:51 -0300

    Adicionado cronometro que ao zerar bloqueia o campo de texto.
    Signed-off-by: Victor <victorcapelini@hotmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,29 @@
 var frase = $(".frase").text();
-var numeroDePalavras = frase.split(/\S+/).length-1;
+var numeroDePalavras = frase.split(/\S+/).length - 1;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numeroDePalavras);
 
 var campo = $(".campo-digitacao");
 
-campo.on("input",function(){
+campo.on("input", function () {
     var conteudo = campo.val();
 
-    var quantidadePalavras = conteudo.split(/\S+/).length-1;
+    var quantidadePalavras = conteudo.split(/\S+/).length - 1;
 
     $("#contador-palavras").text(quantidadePalavras);
     $("#contador-caracteres").text(conteudo.length);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+
+campo.one("focus", function () {
+    var cronometroID = setInterval(function () {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
 });
