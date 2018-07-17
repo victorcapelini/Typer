130a9bcb1682ce53c64e633a42aa64a18d072110
Victor <victorcapelini@hotmail.com>
2018-07-17 14:44:35 -0300

    Adicionado spinner e informado quando a requisição falha.

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,12 +1,20 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases",trocaFraseAleatoria);
+    $("#spinner").toggle(); //mostrando o spinner
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria).fail(function () {
+        $("#erro").toggle();
+        setTimeout(function () {
+            $("#erro").toggle();
+        }, 2500);
+    }).always(function () { //sempre escondendo o spinner
+        $("#spinner").toggle();
+    });
 }
 
 function trocaFraseAleatoria(data) {
     var frase = $(".frase");
-    var numeroAleatorio =Math.floor(Math.random()*data.length);
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
